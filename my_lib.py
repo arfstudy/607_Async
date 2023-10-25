3e3e7e000cf48e24019516e1524b017cacd207bf
arfstudy <147665859+arfstudy@users.noreply.github.com>
2023-10-26 04:11:02 +0500

    Работает над ДЗ 25.10.23

---
+++ my_lib.py
@@ -0,0 +1,26 @@
+import requests
+
+from swap_ppl import PARAMS
+
+
+def get_id(person):
+    url = person["url"]
+    u_len = len(url)
+    corr = 0
+    while url[:u_len - corr].endswith('/'):
+        corr += 1
+    p_start = url[:u_len - corr].rfind('/')
+    number = url[p_start + 1: u_len - corr]
+    if number.isnumeric():
+        return number
+    return number + '-не_число'
+
+
+def get_param(json_data, single):
+    for key, val in PARAMS.items():
+        value = []
+        for address in json_data[key]:
+            res = requests.get(address).json()
+            value.append(res[val])
+        single[key] = ", ".join(value)
+    return single
--- swap_ppl.py
+++ swap_ppl.py
@@ -3,46 +3,63 @@ import datetime
 
 import aiohttp
 import requests
+from more_itertools import chunked
 
+from models import Session, SwapiPeople, Base, engine
+from my_lib import get_param, get_id
+
+MAX_CHUNK_SIZE = 10
+KEYS_LIST = ["birth_year", "eye_color", "gender" "hair_color", "height", "homeworld", "mass", "name", "skin_color"]
 PARAMS = {"films": "title", "vehicles": "name", "starships": "name", "species": "name"}
 
 
 async def get_people(people_id):
-    """ Оформим, чтобы функция get_people() могла работать асинхронно. """
     session = aiohttp.ClientSession()
-    #
-    print(f'{people_id=}')                  # Для теста
     response = await session.get(f'https://swapi.dev/api/people/{people_id}')
-    print(f'{people_id=}, {response=}')     # Для теста
     json_data = await response.json()
-    print(f'{people_id=}, {json_data=}')    # Для теста
     await session.close()
-    print(f'{people_id=} - session closed') # Для теста
-    #
     return json_data
 
 
-def get_param(single):
-    for key, val in PARAMS.items():
-        value = []
-        for address in single[key]:
-            res = requests.get(address).json()
-            value.append(res[val])
-        single[key] = ", ".join(value)
-    return single
+async def insert_to_db(people_json_list):
+    async with Session() as session:
+        swapi_people_list = []
+        for json_data in people_json_list:
+            person = dict()
+            for key, val in json_data:
+                if key in KEYS_LIST:
+                    person[key] = val
+            person = get_param(json_data, person)
+            person["person_id"] = get_id(json_data)
+            swapi_people_list.append(SwapiPeople(**person))
+
+        session.add_all(swapi_people_list)
+        await session.commit()
 
 
 async def main():
 
-    # person = get_param(person)
+    async with engine.begin() as con:
+        await con.run_sync(Base.metadata.create_all)
 
-    person_1 = get_people(1)
-    person_2 = get_people(2)
-    person_3 = get_people(3)
-    person_4 = get_people(4)
-    print(person_1, person_2, person_3, person_4)
+    for ids_chunk in chunked(range(1, 91), MAX_CHUNK_SIZE):
+
+        get_people_coros = [get_people(person_id) for person_id in ids_chunk]
+
+        bundle = await asyncio.gather(*get_people_coros)
+
+        asyncio.create_task(insert_to_db(bundle))
+
+    current_task = asyncio.current_task()
+    tasks_sets = asyncio.all_tasks()
+    tasks_sets.remove(current_task)
+
+    await asyncio.gather(*tasks_sets)
+
+    await engine.dispose()
 
 
-start = datetime.datetime.now()
-asyncio.run(main())    # точка входа
-print(datetime.datetime.now() - start)
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+    asyncio.run(main())    # точка входа
+    print(datetime.datetime.now() - start)
